d1c1bf4bfb3c398551d6cc8c164bd904e0d410ac
Flexasaurusrex <108902627+Flexasaurusrex@users.noreply.github.com>
2025-11-18 14:25:18 -0800

    Update contract.ts

--- lib/contract.ts
+++ lib/contract.ts
@@ -129,5 +129,5 @@ export const HAVE_FEYTH_ABI = [
   }
 ] as const;
 
-export const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `0x${string}`;
+export const CONTRACT_ADDRESS = (process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || '0x0000000000000000000000000000000000000000') as `0x${string}`;
 export const CHAIN_ID = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID || '8453');
