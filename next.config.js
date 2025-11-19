98178c28492a4915378f67a25b2fd53f24e246cc
Flexasaurusrex <108902627+Flexasaurusrex@users.noreply.github.com>
2025-11-18 22:35:29 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@react-native-async-storage/async-storage': false,
+      };
+    }
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      net: false,
-      tls: false,
-    };
     return config;
   },
-  // Ignore async-storage warning from MetaMask SDK
-  transpilePackages: ['@metamask/sdk'],
 };
 
 module.exports = nextConfig;
